bf197bf38f690a794a31252cfec6c67d690c3c59
Veronika Kalivodova <96333440+Kalivodova@users.noreply.github.com>
2021-12-29 19:41:32 +0100

    Update Projekt_ve_VSCode.sql

--- Projekt_ve_VSCode.sql
+++ Projekt_ve_VSCode.sql
@@ -23,8 +23,6 @@ SELECT
 	tpc.*,
 	c.population,
 	CASE WHEN WEEKDAY(tpc.date) IN (5, 6) THEN 1 ELSE 0 END AS weekend,
--- tabulka seasons nemá všechny záznamy ohledně ročního období.
--- s.seasons,
 	CASE WHEN (date BETWEEN '2019-12-21' AND '2020-03-19') OR (date BETWEEN '2020-12-21' AND '2021-03-19') OR (date BETWEEN '2021-12-21' AND '2022-03-19') THEN 0
     WHEN (date BETWEEN '2019-03-20' AND '2019-06-20') OR (date BETWEEN '2020-03-20' AND '2020-06-20') OR (date BETWEEN '2021-03-20' AND '2021-06-20')  OR (date BETWEEN '2022-03-20' AND '2022-06-20') THEN 1
     WHEN (date BETWEEN '2019-06-21' AND '2019-09-22') OR (date BETWEEN '2020-06-21' AND '2020-09-22') OR (date BETWEEN '2021-06-21' AND '2021-09-22') OR (date BETWEEN '2022-06-21' AND '2022-09-22') THEN 2
@@ -36,5 +34,51 @@ LEFT JOIN countries AS c
 ON tpc.country = c.country
 ORDER BY date, country;
 
+-- HDP na hlavu a populace 2018, 2019 a 2020, gini a dětská úmrtnost 2018 a 2019 (roky 2020 nejsou uvedena žádná data).
+CREATE TABLE tabulka_HDP_popu_gini_morta AS 
+SELECT 
+	country,
+	(SELECT round(GDP / population, 2) FROM economies e2 WHERE year = '2018' AND e.country = e2.country) AS HDP_na_hlavu_2018,
+	round(GDP / population, 2) AS HDP_na_hlavu_2019,
+	(SELECT round(GDP / population, 2) FROM economies e2 WHERE year = '2020' AND e.country = e2.country) AS HDP_na_hlavu_2020,
+	(SELECT population FROM economies e2 WHERE year = '2018' AND e.country = e2.country) AS population_2018,
+	population AS population_2019,
+	(SELECT population FROM economies e2 WHERE year = '2020' AND e.country = e2.country) AS population_2020,
+	(SELECT gini FROM economies e2 WHERE year = '2018' AND e.country = e2.country) AS gini_2018,
+	gini AS gini_2019,
+	(SELECT round(mortaliy_under5, 3) FROM economies e2 WHERE year = '2018' AND e.country = e2.country) AS children_mortality_2018,
+	mortaliy_under5 AS children_mortality_2019
+FROM economies e
+WHERE year = '2019';
+
+-- podíl náboženství
+CREATE TABLE tabulka_podil_nabozenstvi AS
+SELECT
+	r.year,
+	r.country,
+	r.religion,
+ 	-- (select sum(re.population) FROM religions re WHERE re.year = '2020' and r.country = re.country group by re.country) AS pocet,
+ 	round(r.population / (SELECT sum(re.population) FROM religions re WHERE re.year = '2020' AND r.country = re.country GROUP BY re.country) * 100, 2) AS procenta
+FROM religions AS r
+WHERE r.year = '2020';
+
+-- rozdíl doby dožití
+CREATE TABLE tabulka_le_rozdil AS 
+SELECT a.country, a.life_exp_1965 , b.life_exp_2015,
+    round( b.life_exp_2015 - a.life_exp_1965, 2 ) AS life_exp_rozdil
+FROM (
+    SELECT le.country , le.life_expectancy AS life_exp_1965
+    FROM life_expectancy le 
+    WHERE year = 1965
+    ) a JOIN (
+    SELECT le.country , le.life_expectancy AS life_exp_2015
+    FROM life_expectancy le 
+    WHERE year = 2015
+    ) b
+    ON a.country = b.country;
+
 SELECT * FROM tabulka_druha_cast;
+SELECT * FROM tabulka_hdp_popu_gini_morta;
+SELECT * FROM tabulka_podil_nabozenstvi;
+SELECT * FROM tabulka_le_rozdil tlr;
 
